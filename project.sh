087728ef8dbb3e943f7a564c0d5f105a33db9228
anurag003 <anuragsinhab@gmail.com>
2016-01-15 17:05:34 +0530

    #3 Implemented changes to take user input.

--- project.sh
+++ project.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+echo "Enter IP Adress for online status : "
+read IP
+(
 a="100% packet loss"
 while [[ "$a" =~ "100% packet loss" ]]
 do
-a=`ping 192.168.1.108 -c 1`
+a=`ping $IP -c 1`
 done
 notify-send "Your Friend is Online."
 exit 0
+) &
